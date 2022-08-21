61a46818026e0037fa43cd989bb44c2d3832cd71
nalsuwailem <nalsuwailem@scai.sa>
2022-08-21 15:35:54 +0300

    optimize imports

--- src/main/java/com/nalsuwailem/be1/controller/UserController.java
+++ src/main/java/com/nalsuwailem/be1/controller/UserController.java
@@ -1,8 +1,6 @@
 package com.nalsuwailem.be1.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
